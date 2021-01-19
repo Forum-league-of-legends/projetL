a127eb8ddbbe3964e9c6a10250960b7ac01c0577
mat42 <mat42@DESKTOP-G4FEKB4.local>
2021-01-19 21:02:28 +0100

    Avancement mvc

--- model/search_all_posts.php
+++ model/search_all_posts.php
@@ -1,9 +1,11 @@
 <?php
 
-    function search_all_posts($test) {
+    function search_all_posts($conn) {
+    	$nom_auteur="";
+   		$titre_topic="":
+    	$reponse = $conn->query('select users.username, topic.titre, topic.date, topic.article, topic.id from users inner join topic on users.id = topic.id_users ');
+    	$results = $reponse->fetchAll();
 
-    $reponse = $test->query('select AU.nom, AR.titre, AR.description, AR.id from auteurs as AU inner join articles as AR on AU.id = AR.id_auteur ');
-    $results = $reponse->fetchAll();
 
     return $results;
     }
