eace910feaa3692e571f3f32cab47b17a3555e3d
mat42 <mat42@DESKTOP-G4FEKB4.local>
2021-02-26 13:59:53 +0100

    reparation date

--- model/publierpost.php
+++ model/publierpost.php
@@ -3,7 +3,7 @@ if (isset($_POST['titre'],$_POST['contenu'])){
     $titre=htmlspecialchars($_POST['titre']);
     $contenu = htmlentities($_POST['contenu'], ENT_QUOTES ,"UTF-8");
     $id_auteur=$_SESSION['id'];
-    $reponse = $conn->prepare('INSERT INTO topic(titre,article,id_users,category) VALUES (?,?,?,0)');
+    $reponse = $conn->prepare('INSERT INTO topic(titre,article,date,id_users,category) VALUES (?,?,NOW(),?,0)');
     $reponse->execute(array($titre,$contenu,$id_auteur));
 
     }
